4726c3173c1b7959428078026dd55aec46404be1
Ilya Fateev <fut33v@ovz1.fat32nov.1y8yz.vps.myjino.ru>
2017-04-18 01:26:27 -0400

    update in get_photos for infinite loop

--- get_photos.py
+++ get_photos.py
@@ -2,6 +2,7 @@ import json
 
 from broadcast import broadcast_message
 from util import bot_util
+import time
 
 _DATA_DIRNAME = "data/"
 _PREVIOUS_PHOTO_DATE_FILENAME = _DATA_DIRNAME + 'previous_photo_date'
@@ -62,21 +63,26 @@ def get_latest_for_album(owner_id, album_id):
         if 'id' in latest_item:
             id = str(latest_item['id'])
 
-        latest_product = text + "\n" + photo_url + "\n\n" +"https://vk.com/id" + user_id
-        # latest_product += "Photo: https://vk.com/photo" + owner_id + "_" + id
-        print latest_product
+        # latest_product = text + "\n" + photo_url + "\n\n" +"https://vk.com/id" + user_id
+        latest_product = photo_url + "\n\nhttps://vk.com/id" + user_id + "\n"
+        latest_product += "\n\nhttps://vk.com/photo" + owner_id + "_" + id
+        # print latest_product
         return latest_product
 
 
 if __name__ == "__main__":
-    with open(_ALBUMS_FILENAME, "r") as albums_file:
-        lines = albums_file.readlines()
-        for l in lines:
-            l = l[:-1]
-            oa_id = l.split('_')
-            if len(oa_id) < 2:
-                continue
-            owner_id = oa_id[0]
-            album_id = oa_id[1]
-            p = get_latest_for_album(owner_id, album_id)
+    while(True):
+        with open(_ALBUMS_FILENAME, "r") as albums_file:
+            lines = albums_file.readlines()
+            for l in lines:
+                l = l[:-1]
+                oa_id = l.split('_')
+                if len(oa_id) < 2:
+                    continue
+                owner_id = oa_id[0]
+                album_id = oa_id[1]
+                print owner_id, album_id
+                p = get_latest_for_album(owner_id, album_id)
             broadcast_message(p)
+        time.sleep(30)
+        print "tick"
